f070cc4bc81c8f0577f36b9fb725944952d3b8f9
asmafaidi28-prog <asmafaidi28@gmail.com>
2025-08-30 02:33:36 +0400

    Update styling (1).js

--- styling (1).js
+++ styling (1).js
@@ -7,7 +7,7 @@
         /* Expose design tokens you can override in SAC styling panel */
         --sac-gauge-tick: #6b7280;
         --sac-gauge-label: var(--sapTextColor, #111);
-        --sac-gauge-needle: #111827;
+        --sac-gauge-needle: #FFFFFF;
         --sac-gauge-knob: #111827;
       }
     </style>
