bff7d62472901a2128ef5a0df94e4e3ff702e104
asmafaidi28-prog <asmafaidi28@gmail.com>
2025-10-08 14:43:57 +0400

    Update mainBAR.js

--- mainBAR.js
+++ mainBAR.js
@@ -95,21 +95,21 @@ var getScriptPromisify = (src) => {
           type: 'bar',
           showBackground: true,
           itemStyle: {
-            color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [
-              { offset: 0, color: '#83bff6' },
-              { offset: 0.5, color: '#188df0' },
-              { offset: 1, color: '#188df0' }
-            ])
-          },
-          emphasis: {
-            itemStyle: {
-              color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [
-                { offset: 0, color: '#2378f7' },
-                { offset: 0.7, color: '#2378f7' },
-                { offset: 1, color: '#83bff6' }
-              ])
-            }
-          },
+  color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [
+    { offset: 0, color: '#6d6d6d' },
+    { offset: 0.5, color: '#6d6d6d' },
+    { offset: 1, color: '#6d6d6d' }
+  ])
+},
+emphasis: {
+  itemStyle: {
+    color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [
+      { offset: 0, color: '#6d6d6d' },
+      { offset: 0.7, color: '#6d6d6d' },
+      { offset: 1, color: '#6d6d6d' }
+    ])
+  }
+},
           data: data
         }
       ]
